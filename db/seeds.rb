71f20dd1fa99dea82a4e03fb39d856126e2afad6
bbrinx <joni.derin@gmail.com>
2017-01-27 22:13:51 +0100

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+# Categories: Bread, Main, Vegetables, Condiments, Desert
+
 Ingredient.create(name: 'cucumber', price: 0.00, amount: 900, cat: 'v')
+Ingredient.create(name: 'tomato', price: 0.00, amount: 900, cat: 'v')
+Ingredient.create(name: 'lettuce', price: 0.00, amount: 900, cat: 'v')
+Ingredient.create(name: 'pickles', price: 0.00, amount: 900, cat: 'v')
+Ingredient.create(name: 'onions', price: 0.00, amount: 900, cat: 'v')
+Ingredient.create(name: 'jalapenos', price: 0.00, amount: 900, cat: 'v')
+Ingredient.create(name: 'meat', price: 500.00, amount: 900, cat: 'm')
+Ingredient.create(name: 'tofu', price: 500.00, amount: 900, cat: 'm')
+Ingredient.create(name: 'ketchup', price: 0.00, amount: 900, cat: 'c')
+Ingredient.create(name: 'mayo', price: 0.00, amount: 900, cat: 'c')
+Ingredient.create(name: 'garlic', price: 0.00, amount: 900, cat: 'c')
+Ingredient.create(name: 'sweet-chilli', price: 0.00, amount: 900, cat: 'c')
+Ingredient.create(name: 'cupcake', price: 150.00, amount: 900, cat: 'd')
+Ingredient.create(name: 'wholegrain', price: 0.00, amount: 900, cat: 'b')
+Ingredient.create(name: 'honeyoat', price: 0.00, amount: 900, cat: 'b')
+Ingredient.create(name: 'sesame', price: 0.00, amount: 900, cat: 'b')
+Ingredient.create(name: 'plain', price: 0.00, amount: 900, cat: 'b')
